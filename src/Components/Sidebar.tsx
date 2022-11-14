3bcad7745b3bce9295d75407e77547d153cf7e1b
Allysson Ribeiro <allyssonribeiro1@gmail.com>
2022-11-14 17:13:24 -0300

    styling sidebar

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -1,17 +1,18 @@
 import styles from "./Sidebar.module.css";
 import perfilImg from "../assets/glenn-carstens-peters-P1qyEf1g0HU-unsplash.jpg";
-import{PencilSimpleLine}from'phosphor-react'
+import { PencilSimpleLine } from "phosphor-react";
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
       <img src={perfilImg} className={styles.cover} alt="perfil" />
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/Allyssonr.png" />
         <strong>Guilherme Navarro</strong>
         <span>Designer UX</span>
       </div>
       <footer>
         <a href="#">
-          <PencilSimpleLine size={20}/>
+          <PencilSimpleLine size={20} />
           Editar seu perfil
         </a>
       </footer>
