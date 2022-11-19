33a946bfff42646a99a657c3f5adf8abb815cf6e
Allysson Ribeiro <allyssonribeiro1@gmail.com>
2022-11-18 22:26:47 -0300

    styliing post

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Header } from "./Components/Header";
-import { Post } from "./post";
+import { Post } from "./Components/Post";
 import styles from "./App.module.css";
 import { Sidebar } from "./Components/Sidebar";
 export function App() {
@@ -9,14 +9,9 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar/>
         <main>
-          <Post
-            title="Diego Fernandes"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, reiciendis est. Neque, dolorum corrupti officiis provident dicta ratione est, optio voluptatum illo tempora quos qui saepe voluptas delectus ullam repellat."
-          />
-          <Post
-            title="Gabriel Buzzi"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, reiciendis est. Neque, dolorum corrupti officiis provident dicta ratione est, optio voluptatum illo tempora quos qui saepe voluptas delectus ullam repellat."
-          />
+          <Post/>
+          <Post/>
+
         </main>
       </div>
     </div>
