b9d0ab00f28b78e9ab4d784d161da04a68081c9e
Allysson Ribeiro <allyssonribeiro1@gmail.com>
2022-12-01 22:32:45 -0300

    create section comment and that css

---
+++ src/Components/Comment.tsx
@@ -0,0 +1,26 @@
+import { Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/diego3g.png" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Diego Fernandes</strong>
+              <time title="11 de maio as 8:13" dateTime="2022-05-11 08:13:30">
+                Cerca de 2h atrás
+              </time>
+            </div>
+            <button title="Deletar Comentario">
+              <Trash size={20} />
+            </button>
+          </header>
+          <p>Muito bom Devon,parabens !! 👏👏</p>
+        </div>
+        <footer>Aplaudir</footer>
+      </div>
+    </div>
+  );
+}
